790430217b14f6d829be07370888efb1203574ab
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2024-07-03 17:15:56 +0100

    feat:home page

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus quasi laboriosam architecto quod, repudiandae reprehenderit quas, sed mollitia molestias nam numquam? Nulla eum, quia itaque molestias aspernatur reiciendis optio laboriosam! Consequuntur voluptas eum eius inventore cum libero earum doloremque explicabo nam? Illum, tempore? Corporis excepturi dolorum ea quos modi nihil assumenda pariatur voluptatum delectus. Animi ipsam debitis eius sit tempore expedita?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
